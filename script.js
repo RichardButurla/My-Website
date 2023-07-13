e220c64e8a5950d2571bedd5c5072409158fc5a0
RichardButurla <rj.buturla@gmail.com>
2023-07-13 18:38:19 +0100

    gif to vid

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-const gifElement = document.querySelector('.project-gif img');
+const vidElement = document.getElementById('game-vid');
 const infoElement = document.querySelector('.project-info');
 let currentProject = 1;
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Play the video when it becomes visible
+      entry.target.play();
+    } else {
+      // Pause the video when it is not visible
+      entry.target.pause();
+    }
+  });
+});
+
+// Observe the video element
+observer.observe(vidElement);
+
 function changeProject() {
   if (currentProject === 1) {
-    gifElement.src = 'test.gif';
+    vidElement.src = 'JointProjectShowcase.mp4';
     infoElement.innerHTML = `
       <h2>Game Title 1</h2>
       <p>Description of Super awesome game</p>
     `;
     currentProject = 2;
   } else if (currentProject === 2) {
-    gifElement.src = 'test2.gif';
+    vidElement.src = 'Ui_Showcase.mp4';
     infoElement.innerHTML = `
       <h2>Game Title 2</h2>
       <p>Description of my next best game</p>
     `;
     currentProject = 3;
   } else if (currentProject === 3) {
-    gifElement.src = 'test3.gif';
+    vidElement.src = 'test3.gif';
     infoElement.innerHTML = `
       <h2>Game Title 3</h2>
       <p>Description of the absolute best game</p>
