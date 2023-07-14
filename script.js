da7d01649c9a18102b8ef243f0dbce517be60b37
RichardButurla <rj.buturla@gmail.com>
2023-07-14 15:48:30 +0100

    links and restyle

--- script.js
+++ script.js
@@ -22,9 +22,13 @@ function changeProject() {
     vidElement.src = 'JointProjectShowcase.mp4';
     infoElement.innerHTML = `
       <h2>Endless Dungeon</h2>
-      <p>My second year final project which was an endless shooter/fighter.
-       This project was programmed using C++ and the SFML library, featuring Melee/Ranged Enemy AI, Breadth-first search algorithim and pathfinding,
-       enemy collision avoidance, different weapon types and behaviours, particle effects and more. This project was lots of fun to make especially
+      <p>My second year final project which was an endless shooter/fighter. </p>
+      <p>You can shoot enemies with different types of weapons like a rifle or shotgun,
+       the enemies continue to spawn as you defeat more of them and collect score. Enemies can also use these weapons and upon defeat they can be dropped 
+       whereas other enemies will drop ammo.
+      <p> This project was programmed using C++ and the SFML library, featuring Melee/Ranged Enemy AI, Breadth-first search algorithim and pathfinding,
+       enemy collision avoidance, different weapon types and behaviours, particle effects and more.</p> 
+       <p> This project was lots of fun to make especially
        implementing the collision avoidance and path finding algorithim. </p>
     `;
     currentProject = 2;
@@ -32,11 +36,11 @@ function changeProject() {
     vidElement.src = 'Ui_Showcase.mp4';
     infoElement.innerHTML = `
       <h2>Mystery Game</h2>
-      <p>My second year project for User Interface Programming. 
-      A mystery themed game where you have to pick up clues and talk to suspects to try solve the crime.
-      Support for Keyboard on Desktop and playable with a Joystick and buttons on mobile.
-      Programmed in Javascript and downloadable as a Progressive Web App, I also made use of HTML, CSS and Local Storage to save progress. 
-      It was fun to make a game in a website within a canvas, connecting HTML, CSS and Javascript and implementing joystick controls.
+      <p>My second year project for User Interface Programming. </p>
+      <p>A mystery themed game where you have to pick up clues and talk to suspects to try solve the crime.</p>
+      <p>Support for Keyboard on Desktop and playable with a Joystick and buttons on mobile.
+      Programmed in Javascript and downloadable as a Progressive Web App, I also made use of HTML, CSS and Local Storage to save progress. </p>
+      <p>It was fun to make a game in a website within a canvas, connecting HTML, CSS and Javascript and implementing joystick controls.
        </p>
     `;
     currentProject = 3;
@@ -44,9 +48,12 @@ function changeProject() {
     vidElement.src = 'Erootica.mp4';
     infoElement.innerHTML = `
       <h2>Plant Dating Simulator</h2>
-      <p>A game me and other students worked on in a game jam. A dating simulator type game where you are a crazy farmer who talks and can develop
-      different types of relationships with his plants with what you say and how you keep them healthy with water and fertiliser.
-      This was developed in GoDot game engine and programmed using GdScript. This was my first time using the engine and it was an awesome experience
+      <p>A game me and other students worked on in a game jam.</p>
+      <p> A dating simulator type game where you are a crazy farmer who talks and can develop
+      different types of relationships with his plants with what you say and how you keep them healthy with water and fertiliser. </p>
+      <p> The different relationships with the flowers was based on a small but neat karma and health system where depending on how healthy a plant is,
+      they can be more or less influenced by your actions including chosen dialogue in the dialogue system.
+      <p> This was developed in GoDot game engine and programmed using GdScript. This was my first time using the engine and it was an awesome experience
       to learn a new language, use new tools, and develop something very unique. </p>
     `;
     currentProject = 1;
